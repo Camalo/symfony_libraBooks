e3fb1345e800e2141be05d619c2b204547b505df
Camalo <60464321+Camalo@users.noreply.github.com>
2020-10-27 20:04:14 +0300

    Update NumbController.php
    добавление входного параметра request

--- NumbController.php
+++ NumbController.php
@@ -2,21 +2,23 @@
 
 namespace App\Controller;
 
-
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class NumbController
 {
     /**
      * @Route("/numb", name="numb")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        $num = mt_rand(0,100);
+        $greet ='';
+        if ($name = $request->query->get('hello')) {
+                        $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
+                 }
+        return new Response(" <html><body>$greet<img src='/images/under-construction.gif' /></body></html>");
 
-        return new Response(
-            '<html><body>The random number is '.$num.'</body></html>'
-        );
     }
 }
